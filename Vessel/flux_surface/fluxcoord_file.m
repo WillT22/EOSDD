81b06b29eba03f50c2c25e4d56850e0c52defdaf
wteague <williamt.mine@gmail.com>
2021-07-02 11:31:44 -0400

    created a data file compiler for torus vessel data

--- Vessel/flux_surface/fluxcoord_file.m
+++ Vessel/flux_surface/fluxcoord_file.m
@@ -1,7 +1,7 @@
 % puts the cylindrical coordinates made by the flux_coordinates function into a .dat file
 
-fileID = fopen('SULI21/Vessel/flux_surface/flux_coords.dat','w');            % opens the file for modification
-fprintf(fileID, 'Flux Surface Cylindrical Coordinates\n\n');    % makes the header
+fileID = fopen('SULI21/Vessel/flux_surface/flux_coords.dat','w');   % opens the file for modification
+fprintf(fileID, 'Flux Surface Cylindrical Coordinates\n\n');        % makes the header
 
 arr_index = [1:size(flux_co(10).r)]';                           % creates an array of indecies
 file_data = [arr_index, flux_co(10).r, arr_index,...            % creates an array of alternating index columns and coordinate columns
---
+++ Vessel/torus_data.m
@@ -0,0 +1,19 @@
+% saves torus vertice/face data as a .dat file
+
+fileID_t = fopen('SULI21/Vessel/torus_vessel.dat','w');           % opens the file for modification
+
+vessel_name = 'NCSX Ex Torus Vessel';
+date = datestr(now, 'mmm-dd-yyyy');
+vertice_total = size(torus.vertices,1);                           % creates an array of indecies
+face_total = size(torus.faces,1);
+
+% prints to the file
+    % HEADER:
+    fprintf(fileID_t, 'MACHINE: %s \n', vessel_name);   % prints the macine name
+    fprintf(fileID_t, 'DATE: %s \n', date);
+    fprintf(fileID_t, 'Vertices: %.0d   Faces: %.0d\n\n', vertice_total, face_total);  
+    % DATA:
+    fprintf(fileID_t, '%20.10e %20.10e %20.10e\n', torus.vertices'); % sets up the format for vertex data
+    fprintf(fileID_t, '%6.0d %6.0d %6.0d\n', torus.faces');
+    fprintf(fileID_t, '/');
+fclose(fileID_t); % closes the file
