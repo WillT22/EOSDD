dfd42558afd13ec4c64623e10475a92cf7c8aa09
wteague <williamt.mine@gmail.com>
2021-11-15 11:46:14 -0500

    created a program to import hitpoint data to python

---
+++ Python/hitpoints.py
@@ -0,0 +1,65 @@
+import numpy as np
+import h5py
+
+
+fldlns_Cbar1_f_05 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_f_05.h5','r');
+fldlns_Cbar2_f_05 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_f_05.h5','r');
+fldlns_Cbar3_f_05 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_f_05.h5','r');
+fldlns_Cbar4_f_05 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_f_05.h5','r');
+fldlns_Cbar5_f_05 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_f_05.h5','r');
+
+fldlns_Cbar1_r_05 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_r_05.h5','r');
+fldlns_Cbar2_r_05 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_r_05.h5','r');
+fldlns_Cbar3_r_05 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_r_05.h5','r');
+fldlns_Cbar4_r_05 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_r_05.h5','r');
+fldlns_Cbar5_r_05 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_r_05.h5','r');
+
+
+fldlns_Cbar1_f_10 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_f_10.h5','r');
+fldlns_Cbar2_f_10 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_f_10.h5','r');
+fldlns_Cbar3_f_10 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_f_10.h5','r');
+fldlns_Cbar4_f_10 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_f_10.h5','r');
+fldlns_Cbar5_f_10 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_f_10.h5','r');
+
+fldlns_Cbar1_r_10 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_r_10.h5','r');
+fldlns_Cbar2_r_10 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_r_10.h5','r');
+fldlns_Cbar3_r_10 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_r_10.h5','r');
+fldlns_Cbar4_r_10 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_r_10.h5','r');
+fldlns_Cbar5_r_10 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_r_10.h5','r');
+
+fldlns_Cbar1_f_15 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_f_15.h5','r');
+fldlns_Cbar2_f_15 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_f_15.h5','r');
+fldlns_Cbar3_f_15 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_f_15.h5','r');
+fldlns_Cbar4_f_15 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_f_15.h5','r');
+fldlns_Cbar5_f_15 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_f_15.h5','r');
+
+fldlns_Cbar1_r_15 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_r_15.h5','r');
+fldlns_Cbar2_r_15 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_r_15.h5','r');
+fldlns_Cbar3_r_15 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_r_15.h5','r');
+fldlns_Cbar4_r_15 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_r_15.h5','r');
+fldlns_Cbar5_r_15 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_r_15.h5','r');
+
+fldlns_Cbar1_f_20 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_f_20.h5','r');
+fldlns_Cbar2_f_20 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_f_20.h5','r');
+fldlns_Cbar3_f_20 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_f_20.h5','r');
+fldlns_Cbar4_f_20 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_f_20.h5','r');
+fldlns_Cbar5_f_20 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_f_20.h5','r');
+
+fldlns_Cbar1_r_20 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_r_20.h5','r');
+fldlns_Cbar2_r_20 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_r_20.h5','r');
+fldlns_Cbar3_r_20 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_r_20.h5','r');
+fldlns_Cbar4_r_20 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_r_20.h5','r');
+fldlns_Cbar5_r_20 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_r_20.h5','r');
+
+fldlns_Cbar1_f_25 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_f_25.h5','r');
+fldlns_Cbar2_f_25 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_f_25.h5','r');
+fldlns_Cbar3_f_25 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_f_25.h5','r');
+fldlns_Cbar4_f_25 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_f_25.h5','r');
+fldlns_Cbar5_f_25 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_f_25.h5','r');
+
+fldlns_Cbar1_r_25 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_r_25.h5','r');
+fldlns_Cbar2_r_25 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_r_25.h5','r');
+fldlns_Cbar3_r_25 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_r_25.h5','r');
+fldlns_Cbar4_r_25 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_r_25.h5','r');
+fldlns_Cbar5_r_25 = h5py.File('../../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_r_25.h5','r');
+
