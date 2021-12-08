97b4d34eb0192c57cf9de2308f3013b8678b0f23
wteague <williamt.mine@gmail.com>
2021-12-08 10:17:28 -0500

    imported Phi variable from hitpoints

--- Python/theta_coordinate.py
+++ Python/theta_coordinate.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.optimize import least_squares
+import hitpoints as hp # imports the hit point data loaded into python from h5 files
+import phi_lines_import as phi_lines # imports PHI_lines as a usable variable
 #import theta_approx as ta # imports theta approximations found in the theta_approx file
 
 # read and extract Fourier coeffecients from nescin file
@@ -8,9 +10,9 @@ M = fourier_cell[:,0]
 N = fourier_cell[:,1]
 crc2 = fourier_cell[:,2]
 czs2 = fourier_cell[:,3]
-crs2 = fourier_cell[:,4]
-czc2 = fourier_cell[:,5]
-
+#importing Phi coordtinate data from hitpoint data set
+Phi_h = phi_lines.Phi[:];
+Phi_h = Phi_h.flatten();
 
 # creating functions for R_s and Z_s
 def R_s(Theta_s,M,N,Phi_h):  
@@ -20,18 +22,18 @@ def R_s(Theta_s,M,N,Phi_h):
     for coord in range(len(Phi_h)):
         for mode in range(len(M)):            
             # Using s=3 and p=1
-            R_s_arr[coord,mode] = crc2[coord] * np.cos(M[mode]*Theta_s[coord] + 3*N[mode]*Phi_h[coord]);    
+            R_s_arr[coord,mode] = crc2[mode] * np.cos(M[mode]*Theta_s[coord] + 3*N[mode]*Phi_h[coord]);    
         R_s_functions[coord] = sum(R_s_arr[coord]);  
     return
     
 def Z_s(Theta_s,M,N,Phi_h):  
-  #initializing arrays for storing outputs of individual elements and summations
+    #initializing arrays for storing outputs of individual elements and summations
     Z_s_arr = np.empty([len(Phi_h),len(M)]); 
     Z_s_functions = np.empty(len(Phi_h));
     for coord in range(len(Phi_h)):
         for mode in range(len(M)):            
             # Using s=3 and p=1
-            Z_s_arr[coord,mode] = czs2[coord] * np.sin(M[mode]*Theta_s[coord] + 3*N[mode]*Phi_h[coord]);    
+            Z_s_arr[coord,mode] = czs2[mode] * np.sin(M[mode]*Theta_s[coord] + 3*N[mode]*Phi_h[coord]);    
         Z_s_functions[coord] = sum(Z_s_arr[coord]);
     return
     
@@ -68,13 +70,11 @@ def chi_squared_jac(Theta_s):
     return np.array([R_s_deriv(Theta_s,M,N,Phi_h)],[Z_s_deriv(Theta_s,M,N,Phi_h)])
 
 # resizing array of approximate thetas into a 1 x n matrix
-	# for bypasing saved data file and using the function directly use the following line
-		#Theta_approx = ta.Theta_approx[:];
 Theta_approx = np.loadtxt('Theta_approx_file.dat')
-print(Theta_approx.shape);
+# for bypasing saved data file and using the function directly use the following line instead
+#Theta_approx = ta.Theta_approx[:];
+# flattens Theta_approx into a 1D matrix for use in least_squares
 Theta0 = Theta_approx.flatten();
-print(Theta0.shape);
-print(Theta0.ndim);
     
 # using the least squares method to find the closest Theta_s
 Theta_s_result = least_squares(chi_squared, Theta0, chi_squared_jac, method='lm');
