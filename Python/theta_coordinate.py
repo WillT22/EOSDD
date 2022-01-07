60e7e2b6a1faafdcd56001025384d4baa310ff5d
wteague <williamt.mine@gmail.com>
2022-01-06 22:43:56 -0500

    Preparing to move coord for loop to encompass least_squares function

--- Python/theta_coordinate.py
+++ Python/theta_coordinate.py
@@ -34,7 +34,7 @@ def R_s(Theta_s,M,N,Phi_h):
     
 def Z_s(Theta_s,M,N,Phi_h):  
     #initializing arrays for storing outputs of individual elements and summations
-    print('calculating Z_s') 
+    print('computing Z_s') 
     Z_s_arr = np.empty([len(Phi_h),len(M)]); 
     Z_s_functions = np.empty(len(Phi_h));
     for coord in range(len(Phi_h)):
@@ -46,39 +46,39 @@ def Z_s(Theta_s,M,N,Phi_h):
     
 # defining the function that will be used in the least squares method
 def chi_squared(Theta_s):
-    print('computing chi_squared')
-    return np.array([np.subtract(R_s(Theta_s,M,N,Phi_h),R_h)],[np.subtract(Z_s(Theta_s,M,N,Phi_h),Z_h)])
+    print('computing chi_squared') 
+    return np.array([np.subtract(R_s(Theta_s,M,N,Phi_h),R_h),np.subtract(Z_s(Theta_s,M,N,Phi_h),Z_h)])
 
 # defining the derivatives with repsect to Theta_s of R_s
 def R_s_deriv(Theta_s,M,N,Phi_h):
-    print('calculating R_s_deriv') 
+    print('computing R_s_deriv') 
     #initializing arrays for storing outputs of individual elements and summations
     R_s_deriv_arr = np.empty([len(Phi_h),len(M)]);
     R_s_deriv_functions = np.empty(len(Phi_h));
     for coord in range(len(Phi_h)):
         for mode in range(len(M)):
             # Using s=3 and p=1
-            R_s_deriv_arr[coord,mode] = -crc2[coord] * M[mode] * np.sin(M[mode]*Theta_s[coord] + 3*N[mode]*Phi_h[coord]);
+            R_s_deriv_arr[coord,mode] = -crc2[mode] * M[mode] * np.sin(M[mode]*Theta_s[coord] + 3*N[mode]*Phi_h[coord]);
         R_s_deriv_functions[coord] = sum(R_s_deriv_arr[coord]);
     return R_s_deriv_functions
 
 # defining the derivatives with repsect to Theta_s of Z_s
 def Z_s_deriv(Theta_s,M,N,Phi_h):
-    print('calculating Z_s_deriv') 
+    print('computing Z_s_deriv') 
     #initializing arrays for storing outputs of individual elements and summations
     Z_s_deriv_arr = np.empty([len(Phi_h),len(M)]);
     Z_s_deriv_functions = np.empty(len(Phi_h));
     for coord in range(len(Phi_h)):
         for mode in range(len(M)):
             # Using s=3 and p=1
-            Z_s_deriv_arr[coord,mode] = czs2[coord] * M[mode] * np.cos(M[mode]*Theta_s[coord] + 3*N[mode]*Phi_h[coord]);
+            Z_s_deriv_arr[coord,mode] = czs2[mode] * M[mode] * np.cos(M[mode]*Theta_s[coord] + 3*N[mode]*Phi_h[coord]);
         Z_s_deriv_functions[coord] = sum(Z_s_deriv_arr[coord]);
     return Z_s_deriv_functions
 
 # defining the Jacobian that will be used in the least squares method
 def chi_squared_jac(Theta_s):
-    print('calculating chi_squared_jacobian')
-    return np.array([R_s_deriv(Theta_s,M,N,Phi_h)],[Z_s_deriv(Theta_s,M,N,Phi_h)])
+    print('computing chi_squared_jacobian')
+    return np.array([R_s_deriv(Theta_s,M,N,Phi_h)],[Z_s_deriv(Theta_s,M,N,Phi_h)]) 
 
 # resizing array of approximate thetas into a 1 x n matrix
 print('approximating theta')
@@ -90,5 +90,5 @@ Theta0 = Theta_approx.flatten();
     
 # using the least squares method to find the closest Theta_s
 print('finding least square')
-Theta_s_result = least_squares(chi_squared, Theta0, chi_squared_jac, method='lm');
-print(Theta_s_result.x);
+Theta_s_result = least_squares(chi_squared, Theta0, chi_squared_jac, method='lm')
+np.savetxt("Theta_s_result.dat",Theta_s_result.x)
