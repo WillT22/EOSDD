33b52eb7dc0c0dc2aa3ab4ed556338448539a537
wteague <williamt.mine@gmail.com>
2022-03-22 12:11:30 -0400

    created color mapping option for heat flux on the surface

---
+++ Matlab/2d_analysis/plot_2d.m
@@ -0,0 +1,65 @@
+function plot_2d(Phi_lines, Theta_lines, option, data_variance)
+
+%%%%%%%%%%%%%%% Default Parameters %%%%%%%%%%%%%%
+  switch nargin         % creates a few default options
+      case 2 
+          option = 0
+          trig_assing_data = [];
+      case 0         % else throw error
+          error('Phi and Theta are required.')
+  end
+
+%% Creating the mesh grid in 2D %%
+clear grid;
+p=180;
+t=90;
+a = p*t; % number of vertices that will be used
+b = a+p+t+1;
+phi = linspace(0,2*pi,p+1); % partition as measured in the toroidal direction 
+theta = linspace(0,2*pi,t+1);   % partition as measured in the poloidal direction
+[Phi, Theta]=meshgrid(phi(1:p+1),theta(1:t+1)); % creates an array from phi and theta 
+Phi = reshape(Phi,[b,1]);
+Theta = reshape(Theta,[b,1]);
+grid.vertices(:,1) = Phi;
+grid.vertices(:,2) = Theta;
+
+clear faces
+faces.lower(:,1) = [1:b-(t+1)];
+faces.lower(:,2) = [2:b-t];
+faces.lower(:,3) = [t+2:b];
+faces.lower(t+1:t+1:end,:) = [];
+
+faces.upper(:,1) = [2:b-t];
+faces.upper(:,2) = [t+3:b+1];
+faces.upper(:,3) = [t+2:b];
+faces.upper(t+1:t+1:end,:) = [];
+
+grid.faces = reshape([faces.lower(:) faces.upper(:)]', [], 3); % combines upper and lower triangular face arrays using every other row
+
+%% Plotting the Figure %%
+figure;
+
+% plotting the vessel on a 2D grid of Theta vs. Phi
+
+% plotting only the vertices
+    %plot(vessel_coords.vertices(:,1),vessel_coords.vertices(:,2),'.');
+    
+% plotting connected vertices in faces
+if option == 0
+    patch('Faces',grid.faces,'Vertices',grid.vertices, 'EdgeColor', [0.6 0.6 0.6], 'FaceColor', [1 1 1]);
+    hold on
+    plot(Phi_lines, Theta_lines, 'linestyle', 'none', 'marker', '.','color','red');
+elseif option == 1
+    faces_cdata = data_variance.nhp_trig;
+    patch('Faces',grid.faces,'Vertices',grid.vertices,'FaceVertexCData',faces_cdata,'FaceColor','flat','EdgeColor','None');
+end
+
+xticks([0 pi/4 pi/2 3*pi/4 pi 5*pi/4 3*pi/2 7*pi/4 2*pi])
+xticklabels({'0', '\pi/4', '\pi/2', '3\pi/4', '\pi', '5\pi/4', '3\pi/2', '7\pi/4', '2\pi'})
+xlim([0,2*pi]);
+yticks([0 pi/4 pi/2 3*pi/4 pi 5*pi/4 3*pi/2 7*pi/4 2*pi])
+yticklabels({'0', '\pi/4', '\pi/2', '3\pi/4', '\pi', '5\pi/4', '3\pi/2', '7\pi/4', '2\pi'})
+ylim([0,2*pi]);
+daspect([1 1 1]);
+xlabel('Phi');
+ylabel('Theta');
---
+++ Matlab/2d_analysis/plot_2d_setup.m
@@ -0,0 +1,41 @@
+%% Importing hitpoint data from fieldlines %%
+%{
+fldlns_Cbar1_f_10 = read_fieldlines('../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_f_10.h5');
+fldlns_Cbar2_f_10 = read_fieldlines('../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_f_10.h5');
+fldlns_Cbar3_f_10 = read_fieldlines('../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_f_10.h5');
+fldlns_Cbar4_f_10 = read_fieldlines('../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_f_10.h5');
+fldlns_Cbar5_f_10 = read_fieldlines('../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_f_10.h5');
+
+fldlns_Cbar1_r_10 = read_fieldlines('../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar1_r_10.h5');
+fldlns_Cbar2_r_10 = read_fieldlines('../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar2_r_10.h5');
+fldlns_Cbar3_r_10 = read_fieldlines('../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar3_r_10.h5');
+fldlns_Cbar4_r_10 = read_fieldlines('../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar4_r_10.h5');
+fldlns_Cbar5_r_10 = read_fieldlines('../../../p/stellopt/ANALYSIS/wteague/flux_surface/simulations/fieldlines/Cbar/fieldlines_Cbar5_r_10.h5');
+%}
+
+fieldline_file = [fldlns_Cbar1_f_10, fldlns_Cbar1_r_10, fldlns_Cbar2_f_10,...
+    fldlns_Cbar2_r_10, fldlns_Cbar3_f_10, fldlns_Cbar3_r_10,...
+    fldlns_Cbar4_f_10, fldlns_Cbar4_r_10, fldlns_Cbar5_f_10,...
+    fldlns_Cbar5_r_10];
+
+%% PHI Lines
+PHI_lines_10 = [];
+for i = 1:length(fieldline_file)
+    PHI_lines_10 = [PHI_lines_10 fieldline_file(i).PHI_lines(:,2)];
+end
+PHI_lines_10 = reshape(PHI_lines_10, 40000,5);
+for f = 1:size(PHI_lines_10,2)
+    for i = 1:size(PHI_lines_10,1)
+        if PHI_lines_10(i,f) < 0
+			PHI_lines_10(i,f) = PHI_lines_10(i,f) + 2*pi;
+        end
+    end
+end
+%% THETA Lines
+THETA_lines_10 = importdata('EOSDD/Python/Theta_Cbar_10.dat');
+
+%% plot_2d
+plot_2d(PHI_lines_10, THETA_lines_10)
+
+%% Plotting a Color Mapping of Heat Flux on the Surface
+plot_2d(PHI_lines_10, THETA_lines_10,1,data_variance)
